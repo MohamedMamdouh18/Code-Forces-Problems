10dbd8dd1f85824a981521d56c75d0efa8ff02d0
Mohamed Mamdouh <63814228+MohamedMamdouh18@users.noreply.github.com>
2020-12-13 12:19:08 +0200

    Update main.c

--- Level A/Gravity Flip/main.c
+++ Level A/Gravity Flip/main.c
@@ -1,30 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main()
 {
-    int n ;
-    scanf ("%i" , &n) ;
-    int arr[n] ;
-    for(int i = 0 ; i < n ; i ++)
+    int n , pair = 0 ;
+    scanf("%i" , &n) ;
+    char arr[n] ;
+    scanf("%s" , &arr) ;
+    for(int i = 1 ; i < n ; i ++)
     {
-        scanf("%i" , &arr[i]);
-    }
-    for(int i = 0 ; i < n ; i ++)
-    {
-        for(int j = 0 ; j < n - 1 ; j ++)
+        if(arr[i] == arr[i - 1])
         {
-            if(arr[j] > arr[j + 1])
-            {
-                int a = arr[j] ;
-                arr[j] = arr[j + 1] ;
-                arr[j + 1] = a ;
-            }
+            pair ++ ;
         }
     }
-    for(int i = 0 ; i < n ; i ++)
-    {
-        printf("%i " , arr[i]);
-    }
-
+    printf("%i" , pair) ;
 }
