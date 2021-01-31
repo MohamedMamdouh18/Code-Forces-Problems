50735ad26797717e0214c34c79e51185f9cddeee
Mohamed Mamdouh <63814228+MohamedMamdouh18@users.noreply.github.com>
2021-01-31 21:31:01 +0200

    Update main.cpp

--- Level A/Snacktower/main.cpp
+++ Level A/Snacktower/main.cpp
@@ -1,8 +1,17 @@
+
 #include <bits/stdc++.h>
 using namespace std ;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define maxv(a)      (*max_element((a).begin(), (a).end()))
+#define bsrch(a , n)    (binary_search(a.begin() , a.end() , n))
+#define srt(a)     (sort(a.begin() , a.end()))
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define rep(i , a, b)    for(int i = a; i < b; i++)
- 
+
+
 int main() {
     IOS
     int n , num;
@@ -21,11 +30,8 @@ int main() {
                 a.pop() ;
                 big -- ;
             }
-            cout << endl ;
         }
-        else
-        {
-            cout << endl ;
-        }
+        cout << endl ;
     }
+
 }
