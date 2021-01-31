759e802c5e6acf58938b187052fd7d33c49cc18c
Mohamed Mamdouh <63814228+MohamedMamdouh18@users.noreply.github.com>
2021-01-31 21:29:25 +0200

    Update main.cpp

--- Level A/Snacktower/main.cpp
+++ Level A/Snacktower/main.cpp
@@ -1,25 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std ;
-
-
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-
+#define rep(i , a, b)    for(int i = a; i < b; i++)
+ 
 int main() {
     IOS
-    int n , number;
+    int n , num;
     cin >> n ;
-    int biggest = n ;
-    int a[n+1] = {0} ;
-    for(int i = 0 ; i < n ; i ++)
+    int big = n ;
+    priority_queue <int> a ;
+    rep(i , 0 , n)
     {
-        cin >> number ;
-        a[number] = 1 ;
-        while(a[biggest] && biggest != 0)
+        cin >> num ;
+        a.push(num) ;
+        if(a.top() == big)
         {
-            cout << biggest << " " ;
-            biggest -- ;
+            while(!a.empty() && a.top() == big)
+            {
+                cout << a.top() << " " ;
+                a.pop() ;
+                big -- ;
+            }
+            cout << endl ;
         }
-        cout << endl ;
+        else
+        {
+            cout << endl ;
+        }
     }
-
 }
